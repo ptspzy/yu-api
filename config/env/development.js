8c8f5eb3d70d4babf40fa4e2a8466c3ee1e638d2
pzy <ptspzy@126.com>
2017-02-15 21:14:22 +0800

    Init project

---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+const devConfig = {
+  env: 'development',
+  MONGOOSE_DEBUG: true,
+  jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
+  db: 'mongodb://localhost/lagou',
+  port: 4040
+};
+
+export default devConfig;
